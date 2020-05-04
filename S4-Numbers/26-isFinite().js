5627abb334bc9c6980fe44e9c17e844a92c63825
pochiman <pochiman@yahoo.com>
2020-05-04 19:12:17 -0400

    initial commit

---
+++ S4-Numbers/26-isFinite().js
@@ -0,0 +1,8 @@
+let example1 = 1111;
+
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(example1));
+console.log(Number.isFinite('hello'));
+console.log(Number.isFinite('0'));
+console.log(Number.isFinite(0));
+console.log(Number.isFinite(NaN));
