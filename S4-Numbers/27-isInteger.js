4d23e1a9250aba6439e264131d0874ff9df45f3a
pochiman <pochiman@yahoo.com>
2020-05-04 19:27:00 -0400

    initial commit

---
+++ S4-Numbers/27-isInteger.js
@@ -0,0 +1,7 @@
+let example1 = 1111;
+let example2 = 1111.1;
+let example3 = 0.1;
+
+console.log(Number.isInteger(example1));
+console.log(Number.isInteger(example2));
+console.log(Number.isInteger(example3));
