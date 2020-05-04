ecb64b077b938ac94964466c422b87a796afcb87
pochiman <pochiman@yahoo.com>
2020-05-04 15:28:52 -0400

    initial commit

---
+++ S3-String-Methods/18-slice().js
@@ -0,0 +1,10 @@
+let example1 = 'Hello World';
+let example2 = example1.slice(5);
+let example3 = example1.slice(0);
+let example4 = example1.slice(0, 4);
+
+console.log(example1.slice(5));
+console.log(example1.slice());
+console.log(example2);
+console.log(example3);
+console.log(example4);
