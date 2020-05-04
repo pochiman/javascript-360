a28233e91cb99a0d662e85b646f5f892b644e8ea
pochiman <pochiman@yahoo.com>
2020-05-04 19:02:36 -0400

    initial commit

---
+++ S4-Numbers/numberProperties.js
@@ -0,0 +1,8 @@
+let example1 = 1111;
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Number.NaN);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
