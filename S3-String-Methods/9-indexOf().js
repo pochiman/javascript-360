38e5b69b35da62dc4a5a4b3b0b2efabe891c62f4
pochiman <pochiman@yahoo.com>
2020-05-03 22:28:39 -0400

    initial commit

---
+++ S3-String-Methods/9-indexOf().js
@@ -0,0 +1,9 @@
+let example1 = 'Hello World';
+
+console.log(example1.indexOf('e'));
+console.log(example1.indexOf('el'));
+console.log(example1.indexOf('elo'));
+console.log(example1.indexOf('World'));
+console.log(example1.indexOf('world'));
+console.log(example1.indexOf('l'));
+console.log(example1.indexOf('ll'));
