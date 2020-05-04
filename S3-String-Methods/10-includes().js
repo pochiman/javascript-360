17fb4e0d52880f64f578044143d3871e7e328df4
pochiman <pochiman@yahoo.com>
2020-05-04 11:30:10 -0400

    initial commit

---
+++ S3-String-Methods/10-includes().js
@@ -0,0 +1,7 @@
+let example1 = 'Hello World';
+
+console.log(example1.includes('H'));
+console.log(example1.includes('h'));
+console.log(example1.includes('Hello'));
+console.log(example1.indexOf('Hello') === -1);
+console.log(example1.indexOf('Hello') > -1);
