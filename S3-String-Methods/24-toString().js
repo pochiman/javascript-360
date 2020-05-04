6a9741a16da99ee798e145c03328f97de0575fb1
pochiman <pochiman@yahoo.com>
2020-05-04 18:50:31 -0400

    initial commit

---
+++ S3-String-Methods/24-toString().js
@@ -0,0 +1,7 @@
+let example1 = 'Hello, World';
+let example2 = 1111;
+
+console.log(example1.toString());
+console.log(example2.toString());
+console.log( typeof example2.toString());
+console.log( typeof example2);
