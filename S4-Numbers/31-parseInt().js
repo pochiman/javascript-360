1cb677b41a45390c5f11360726b688359844d28e
pochiman <pochiman@yahoo.com>
2020-05-05 00:00:59 -0400

    initial commit

---
+++ S4-Numbers/31-parseInt().js
@@ -0,0 +1,13 @@
+let example1 = 1111;
+let example2 = 1111.1111;
+let example3 = '1111.1111';
+let example4 = 'to 1111.1111';
+let example5 = '1111 to';
+let example6 = 'to 1111 to';
+
+console.log(parseInt(example1));
+console.log(parseInt(example2));
+console.log(parseInt(example3));
+console.log(parseInt(example4));
+console.log(parseInt(example5));
+console.log(parseInt(example6));
