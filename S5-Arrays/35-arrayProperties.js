bc8bb65653e46d63e7d4a1112573a105ab606a99
pochiman <pochiman@yahoo.com>
2020-05-05 15:08:18 -0400

    initial commit

---
+++ S5-Arrays/35-arrayProperties.js
@@ -0,0 +1,11 @@
+let example1 = [];
+let example2 = [1, 2, 3, true, 'Hello', {hi: true}];
+
+console.log(example1.length);
+console.log(example2);
+console.log(typeof example2);
+console.log(typeof {});
+console.log(Array.isArray(example1));
+console.log(Array.isArray(true));
+console.log(Array.isArray(1));
+console.log(Array.isArray({}));
