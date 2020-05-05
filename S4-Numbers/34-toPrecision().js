ab4703c7fdf8f2b5a04bed9c586cea90a0dd6288
pochiman <pochiman@yahoo.com>
2020-05-05 14:38:07 -0400

    initial commit

---
+++ S4-Numbers/34-toPrecision().js
@@ -0,0 +1,7 @@
+let example1 = 10.225;
+
+console.log(example1.toPrecision(1));
+console.log(example1.toPrecision(2));
+console.log(example1.toPrecision(3));
+console.log(example1.toPrecision(4));
+console.log(example1.toPrecision(5));
