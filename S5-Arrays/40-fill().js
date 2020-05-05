2f73d33e3ba8b3ca2742677c25ffd78c5769a7aa
pochiman <pochiman@yahoo.com>
2020-05-05 19:08:55 -0400

    initial commit

---
+++ S5-Arrays/40-fill().js
@@ -0,0 +1,7 @@
+let example1 = [1, 2, 3, 4, 5];
+let example2 = [1, 2, 3, 4, 5];
+let example3 = [1, 2, 3, 4, 5];
+
+console.log(example1.fill(1));
+console.log(example2.fill(1, 3));
+console.log(example3.fill(1, 3, 4));
