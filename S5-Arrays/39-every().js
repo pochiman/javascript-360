60afb88e660d4f2f8cdabf667d1a4ff7928211d8
pochiman <pochiman@yahoo.com>
2020-05-05 18:57:43 -0400

    initial commit

---
+++ S5-Arrays/39-every().js
@@ -0,0 +1,5 @@
+let example1 = [1, 2, 3, 4, 5].every((element) => {return element > 0});
+let example2 = [1, 2, 3, 4, 5].every((element) => {return element > 1});
+
+console.log(example1);
+console.log(example2);
