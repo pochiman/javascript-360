6786647e765fa1f4515268498048f7d56139e4f5
pochiman <pochiman@yahoo.com>
2020-05-05 14:30:49 -0400

    initial commit

---
+++ S4-Numbers/33-toFixed().js
@@ -0,0 +1,14 @@
+let example1 = 1111.225;
+let example2 = 1111.226;
+let example3 = 1111.2225;
+let example4 = 1111.2226;
+
+console.log(example1.toFixed());
+console.log(example1.toFixed(2));
+console.log(example1.toFixed(3));
+console.log(example2.toFixed(2));
+console.log(example2.toFixed(3));
+console.log(example3.toFixed(3));
+console.log(example3.toFixed(4));
+console.log(example4.toFixed(3));
+console.log(example4.toFixed(4));
