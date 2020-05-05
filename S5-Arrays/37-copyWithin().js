a2b0db0ac27ebe658c55441b9ed9f4b078cd5d34
pochiman <pochiman@yahoo.com>
2020-05-05 16:21:19 -0400

    initial commit

---
+++ S5-Arrays/37-copyWithin().js
@@ -0,0 +1,12 @@
+let example1 = [true, false, 1, 2, 3, 4, 5];
+
+console.log(example1.copyWithin(2));
+console.log(example1.copyWithin(-2));
+console.log(example1.copyWithin(-3));
+console.log(example1.copyWithin(-4));
+console.log(example1.copyWithin(-5));
+console.log(example1.copyWithin(-6));
+console.log(example1.copyWithin(-7));
+console.log(example1.copyWithin(2, 0, example1.length -1));
+console.log(example1.copyWithin(2, 0, example1.length -2));
+console.log(example1.copyWithin(2, 0, example1.length -3));
