6f58bf564b39005bb3091b845073747b9c293d6e
pochiman <pochiman@yahoo.com>
2020-05-06 15:37:48 -0400

    initial commit

---
+++ S5-Arrays/53-shift().js
@@ -0,0 +1,17 @@
+let example1 = [1, 2, 3, 4, 5];
+console.log(example1.shift());
+
+
+
+let example2 = [1, 2, 3, 4, 5];
+example2.shift();
+
+console.log(example2);
+
+
+
+let example3 = [1, 2, 3, 4, 5];
+let example4 = example3.shift();
+
+console.log(example3);
+console.log(example4);
