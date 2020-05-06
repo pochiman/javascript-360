39f1fc038e918eb44ededc819ce47275e2696180
pochiman <pochiman@yahoo.com>
2020-05-06 17:39:08 -0400

    initial commit

---
+++ S6-Operators/59-arithmeticOperators.js
@@ -0,0 +1,15 @@
+function addTwoNumbers(a, b) {
+  
+  return a + b;
+  // return a - b;
+  // return a + b + 2;
+  // return b / a;
+  // return b * a;
+  // return b ** a;
+  // return b % a;
+  // return 9 % a;
+  // return 9 % 4;
+
+}
+
+console.log(addTwoNumbers(2, 8));
