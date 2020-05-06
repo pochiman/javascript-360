1a5fd95baf0d5f71a40b9fcfb35d36d3effc6b80
pochiman <pochiman@yahoo.com>
2020-05-06 15:27:41 -0400

    initial commit

---
+++ S5-Arrays/52-reduce().js
@@ -0,0 +1,9 @@
+let example1 = [1, 2, 3, 4, 5]
+  .reduce((currentTotal, currentValue) => {
+    
+    console.log(currentTotal);
+    return currentTotal + currentValue;
+
+});
+
+console.log(example1);
