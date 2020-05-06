a1e4e7b45fd7a785549f059b1fca6fd028e64bb3
pochiman <pochiman@yahoo.com>
2020-05-05 23:48:51 -0400

    initial commit

---
+++ S5-Arrays/46-indexOf().js
@@ -0,0 +1,8 @@
+let example1 = [1, 2, 3, 4, 5];
+let example2 = [1, 2, 3, 4, 5, 5, 6];
+
+console.log(example1.indexOf(3));
+console.log(example2.indexOf(5));
+console.log(example2.indexOf(10));
+console.log(example2.indexOf(5, 4));
+console.log(example2.indexOf(5, 5));
