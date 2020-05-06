beea1f7f7d422b9e76acca980963160e14c67622
pochiman <pochiman@yahoo.com>
2020-05-06 14:14:16 -0400

    initial commit

---
+++ S5-Arrays/50-pop().js
@@ -0,0 +1,17 @@
+let example1 = [1, 2, 3, 4, 5];
+console.log(example1.pop());
+
+
+
+let example2 = [1, 2, 3, 4, 5];
+example2.pop();
+
+console.log(example2);
+
+
+
+let example3 = [1, 2, 3, 4, 5];
+let example4 = example3.pop();
+
+console.log(example3);
+console.log(example4);
