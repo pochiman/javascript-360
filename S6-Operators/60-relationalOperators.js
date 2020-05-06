1c7c7a3e0b7a705a4554447b878252fef8df2d33
pochiman <pochiman@yahoo.com>
2020-05-06 17:55:16 -0400

    initial commit

---
+++ S6-Operators/60-relationalOperators.js
@@ -0,0 +1,14 @@
+function addTwoNumbers(a, b) {
+  
+  // return a < b;
+  // return a > b;
+  // return a >= b;
+  // return a <= b;
+  // return 'name' in a;
+  return 'name2' in a;
+
+}
+
+// console.log(addTwoNumbers(2, 8));
+// console.log(addTwoNumbers(8, 8));
+console.log(addTwoNumbers({name: 'Poch'}, 8));
