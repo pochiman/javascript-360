7075195b87e0c092f90619bbc40e7616f78dc60b
pochiman <pochiman@yahoo.com>
2020-05-05 22:12:38 -0400

    initial commit

---
+++ S5-Arrays/42-find().js
@@ -0,0 +1,20 @@
+let example1 = [1, 2, 3, 4, 5].find((element) => {
+  return element > 3;
+});
+
+let example2 = [1, 2, 3, 4, 5].find((element) => {
+  return element > 4;
+});
+
+let example3 = [1, 2, 3, 4, 5].find((element) => {
+  return element > 6;
+});
+
+let example4 = [1, 2, 3, 4, 5].find((element) => {
+  return element > 6;
+});
+
+console.log(example1)
+console.log(example2);
+console.log(example3);
+console.log(Boolean(example4));
