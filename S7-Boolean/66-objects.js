e41f92f5c5dcdd5aee1988a3664c0ad0c1a840a5
pochiman <pochiman@yahoo.com>
2020-05-07 11:43:20 -0400

    initial commit

---
+++ S7-Boolean/66-objects.js
@@ -0,0 +1,12 @@
+let example1 = {
+  
+  // 'test': 1, 
+  test: 1, 
+  test3: {hello: 5},
+  test2: 2
+
+};
+
+// console.log(example1.test);
+// console.log(example1['test']);
+console.log(example1.test3);
