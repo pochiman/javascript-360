a18188aa6ffb213c4ff0b3d719c605ce31eab4e0
pochiman <pochiman@yahoo.com>
2020-05-07 10:54:50 -0400

    initial commit

---
+++ S6-Operators/64-increment-decrementOperators.js
@@ -0,0 +1,19 @@
+function addTwoNumbers(a, b) {
+  
+  let example = 5;
+  
+  // example++;
+  // ++example;
+
+  // example--;
+  // --example;
+  
+  // return ++example;
+  // return example++;
+
+  // return --example;
+  return example--;
+
+}
+
+console.log(addTwoNumbers(2, 3));
