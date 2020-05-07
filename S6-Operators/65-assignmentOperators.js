de51f2cd254490d122902af28fadcca0089d6cda
pochiman <pochiman@yahoo.com>
2020-05-07 11:10:39 -0400

    initial commit

---
+++ S6-Operators/65-assignmentOperators.js
@@ -0,0 +1,25 @@
+function addTwoNumbers(a, b) {
+  
+  // let example = 12;
+  let example = 13;
+
+  // example = example + 6;
+  // example += 6;
+
+  // example = example - 6;
+  // example -= 6;
+
+  // example = example * 6;
+  // example *= 6;
+
+  // example = example / 6;
+  // example /= 6;
+
+  // example = example % 6;
+  example %= 6;
+
+  return example;
+
+}
+
+console.log(addTwoNumbers(2, 3));
