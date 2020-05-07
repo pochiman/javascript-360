1a94e1046d743a23e022973c7b28abd3b9731880
pochiman <pochiman@yahoo.com>
2020-05-07 07:51:47 -0400

    initial commit

---
+++ S6-Operators/62-binaryLogicalOperators.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(a, b) {
+  
+  // return 2 === a && 2 < 3;
+  // return 2 === a && 4 < 3;
+  // return 2 === a && 2 < 3 && 2 > 3;
+  return 2 === 3 || b === a;
+
+}
+
+console.log(addTwoNumbers(2, 2));
