2d7955edf6c47ca1e5defce4d2406ad0a8fe76e9
pochiman <pochiman@yahoo.com>
2020-05-07 10:42:03 -0400

    initial commit

---
+++ S6-Operators/63-conditionalTernaryOperator.js
@@ -0,0 +1,18 @@
+function addTwoNumbers(a, b) {
+  
+// let example = a > b ? 'a' : 'b';
+// let example = a > b ? 'a' : (4 > 5) ? true : false;
+// let example = a > b ? 'a' : (4 > 5) ? 'World' : 'Hello';
+// let example = a < b ? 'a' : (4 > 5) ? 'World' : 'Hello';
+// let example = a > b ? 'a' : (4 > 5) ? 'World' : 'Hello';
+// let example = a > b ? 'a' : (4 < 5) ? 'World' : 'Hello';
+// let example = a > b ? 'a' : (4 > 5) ? 'World' : (6 > 7) ? 'Yes' : 'No';
+let example = a > b ? 'a' : (4 > 5) ? 'World' : (6 < 7) ? 'Yes' : 'No';
+
+  // return (2 + 2 === 4) ? 'Hello World' : false;
+  return example;
+
+}
+
+// console.log(addTwoNumbers(2, 1));
+console.log(addTwoNumbers(2, 3));
