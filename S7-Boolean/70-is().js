83b8443483973f9b49a9004c694cf3889aff1201
pochiman <pochiman@yahoo.com>
2020-05-09 19:23:55 -0400

    initial commit

---
+++ S7-Boolean/70-is().js
@@ -0,0 +1,13 @@
+let example1 = {
+  hello: 'World', 
+  bye: 'Now'
+};
+
+// let example2 = example1;
+// let example2 = true;
+let example2 = Object.assign({}, example1);
+
+console.log(Object.is(example1, example2));
+
+console.log(Object.is('Hello', 'hello'));
+console.log(Object.is('Hello', 'Hello'));
