e8dd9f91c077fb854ec6d1a86d536af9b3084efe
pochiman <pochiman@yahoo.com>
2020-05-09 14:42:23 -0400

    initial commit

---
+++ S7-Boolean/68-entries().js
@@ -0,0 +1,8 @@
+let example1 = {hello: 'World', bye: 'Now'}
+
+let example2 = {hello: 'World', bye: 'Now',
+test: {example3: 'example4'}
+};
+
+console.log(Object.entries(example1));
+console.log(Object.entries(example2));
