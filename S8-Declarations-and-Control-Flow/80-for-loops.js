7c2840073339e3c0897290140eccd6f32581ed88
pochiman <pochiman@yahoo.com>
2020-05-10 11:17:46 -0400

    initial commit

---
+++ S8-Declarations-and-Control-Flow/80-for-loops.js
@@ -0,0 +1,12 @@
+function run(input) {
+let exampleArray = [true];
+console.log(exampleArray[0])
+console.log(exampleArray.length)
+  for(let i = 0; i < input; i++) {
+  // for(let i = 0; i < input; i += 2) {
+    console.log(i);
+  }
+
+}
+
+  run(5);
