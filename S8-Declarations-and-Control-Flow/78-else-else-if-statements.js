e4caac7adea233094ba85777260a716668dfc366
pochiman <pochiman@yahoo.com>
2020-05-10 10:18:15 -0400

    initial commit

---
+++ S8-Declarations-and-Control-Flow/78-else-else-if-statements.js
@@ -0,0 +1,26 @@
+// function run(input) {
+//   if(true) {  
+//   // if(false) {  
+//     console.log(input);
+//   } else if(true) {
+//     console.log('E IF')
+//   } else {
+//     console.log('Fail');
+//   }
+// }
+
+
+function run(input) {
+  if(true) {
+    console.log(input);
+  }
+
+  if(false) {
+  // if(true) {
+    console.log(true)
+  } else {
+    console.log('Fail');
+  }
+}
+
+  run(true);
