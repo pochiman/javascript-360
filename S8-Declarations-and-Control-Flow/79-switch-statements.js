3db4fdc72e8e4fee959326ee94fac14f5d748410
pochiman <pochiman@yahoo.com>
2020-05-10 10:31:10 -0400

    initial commit

---
+++ S8-Declarations-and-Control-Flow/79-switch-statements.js
@@ -0,0 +1,20 @@
+function run(input) {
+
+  switch(input) {
+    case 'Code':
+      console.log('Code');
+      break;
+    case 'Cats':
+      console.log('Cats');
+      break;
+    case 'Money':
+      console.log('Money');
+      break;
+    default:
+      console.log('You do not know Poch that well')
+  }
+
+}
+
+  run('Money');
+  // run('People with egos');
