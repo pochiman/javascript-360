267a839e7fab5b0ea0382d06df786d292f7d92d4
pochiman <pochiman@yahoo.com>
2020-05-12 13:40:07 -0400

    initial commit

---
+++ S13-ES6+/113-objectLiteralShorthand.js
@@ -0,0 +1,13 @@
+let first = 'Poch';
+let middleInitial = 'Q';
+let last = 'Tan';
+let codingStatus = 'Godly';
+
+let exampleObject = {
+  first,
+  middleInitial,
+  last,
+  codingStatus
+};
+
+console.log(exampleObject);
