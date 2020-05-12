49c3c7f683062d1948fedc143a3dee4b946e7dbd
pochiman <pochiman@yahoo.com>
2020-05-12 17:37:28 -0400

    initial commit

--- S14-Functions/119-functions.js
+++ S14-Functions/119-functions.js
@@ -1,6 +1,9 @@
+addTwoNumbers(2, 4);
+addTwoNumbers(2, 5);
+
 function addTwoNumbers(a, b) {
   console.log(a + b);
 }
 
-addTwoNumbers(2);
-addTwoNumbers(2, 5);
+// addTwoNumbers(2, 4);
+// addTwoNumbers(2, 5);
---
+++ S14-Functions/120-passingAFunction.js
@@ -0,0 +1,8 @@
+function addTwoNumbers(a, b) {
+  console.log(a + b);
+}
+
+console.log(addTwoNumbers);
+
+let example = addTwoNumbers;
+example(2, 8);
