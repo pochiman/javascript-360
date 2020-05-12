4ec89ee5b347ffed0ea1232e010dbbd2761d8e6c
pochiman <pochiman@yahoo.com>
2020-05-12 19:44:15 -0400

    initial commit

---
+++ S15-Math-Object/125-math.js
@@ -0,0 +1,17 @@
+console.log(Math.PI);
+
+console.log(Math.round(1.6));
+
+console.log(Math.pow(10, 2));
+
+console.log(Math.sqrt(100));
+
+console.log(Math.abs(-100));
+
+console.log(Math.ceil(10.1));
+
+console.log(Math.floor(10.999999));
+
+console.log(Math.min(10, 0, -100, -200, 100));
+
+console.log(Math.max(100, 1000, 10000));
