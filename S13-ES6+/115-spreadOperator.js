ab3b3ed4ed6675fbf96f1881f3358624bba5ce28
pochiman <pochiman@yahoo.com>
2020-05-12 14:17:22 -0400

    initial commit

---
+++ S13-ES6+/115-spreadOperator.js
@@ -0,0 +1,5 @@
+const exampleArray = ['Hello', 'Bye', 'Hi', 'Yolo', 'Sexy Beast'];
+let example2Array = [1, 2, 3];
+example2Array.push(...exampleArray);
+// console.log(...exampleArray);
+console.log(example2Array);
