5c4f8b328978f149714c5cbc1c52b7ee127129f2
pochiman <pochiman@yahoo.com>
2020-05-12 21:25:10 -0400

    initial commit

---
+++ S16-Date/127-date.js
@@ -0,0 +1,13 @@
+// const exDate = new Date();
+// console.log(new Date());
+// console.log(exDate);
+
+// const exDate = new Date().getTime();
+// console.log(exDate);
+// console.log(new Date(exDate));
+
+// const exDate = new Date().toString();
+// console.log(new Date(exDate));
+
+const exDate = new Date().toString();
+console.log(new Date(1999, 00, 01, 12, 20, 25, 10));
