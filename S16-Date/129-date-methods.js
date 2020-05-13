15f144934573a8ba96b23a336b222c247d8a4a23
pochiman <pochiman@yahoo.com>
2020-05-12 22:08:16 -0400

    initial commit

---
+++ S16-Date/129-date-methods.js
@@ -0,0 +1,10 @@
+let exDate = new Date();
+
+console.log(exDate);
+// console.log(exDate.getDate());
+// console.log(exDate.getDay());
+// console.log(exDate.getFullYear());
+// console.log(exDate.getTime());
+
+// exDate.setFullYear(2016);
+console.log(exDate);
